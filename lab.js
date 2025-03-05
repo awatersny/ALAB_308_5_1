7cab92200779b1a0d768fd0cb367ec9512efdd9b
awatersny <awatersny@gmail.com>
2025-03-05 17:11:10 -0500

    Import data for part 2

--- lab.js
+++ lab.js
@@ -68,3 +68,10 @@ console.log(arrAvg(numArr))
 console.log(longest(strArr))
 console.log(moreCharsThan(strArr, 4))
 printNumsFrom(20)
+
+const data = [{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+  { id: "48", name: "Barry", occupation: "Runner", age: "25" },
+  { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+  { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+  { id: "7", name: "Bilbo", occupation: "None", age: "111" }]
+
